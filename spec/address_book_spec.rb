a63fed7a20696ed7870317013659e79b95f5ca18
Joe <jdg006@lvc.edu>
2017-07-21 20:55:43 +0000

    assignment 6 complete

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -114,6 +114,30 @@ describe "#add_entry" do
        entry_five = book.entries[4]
        check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
      end
+     
+     it "imports the correct number of entries from the second csv file" do
+         book.import_from_csv("entries_2.csv")
+         book_size = book.entries.size
+         expect(book_size).to eq 3
+     end
+     
+     it "imports the 1st entry of the second csv file" do
+       book.import_from_csv("entries_2.csv")
+       entry_one = book.entries[0]
+       check_entry(entry_one, "Andy" , "555-555-1111"  , "Andy@blocmail.com")
+     end
+     
+     it "imports the 2nd entry of the second csv file" do
+       book.import_from_csv("entries_2.csv")
+       entry_two = book.entries[1]
+       check_entry(entry_two, "Bill" , "555-555-2222" , "Bill@blocmail.com")
+     end
+     
+     it "imports the 3rd entry of the second csv file" do
+       book.import_from_csv("entries_2.csv")
+       entry_three = book.entries[2]
+       check_entry(entry_three, "Jeremy" , "555-555-3333" , "Jeremy@blocmail.com")
+     end
    
    
    end
