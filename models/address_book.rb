185ad2c158f573d998ed1a2c1d97b7e62185a8de
Joe <jdg006@lvc.edu>
2017-07-25 19:35:36 +0000

    checkpoint 9 complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -30,7 +30,7 @@ class AddressBook
      # #8
      csv.each do |row|
        row_hash = row.to_hash
-       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
  end
  
